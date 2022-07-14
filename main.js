67fa6c8de02039d8bdaf30f0d5e4a12f3eb450d5
Parivesh2005 <108748552+Parivesh2005@users.noreply.github.com>
2022-07-14 11:54:42 -0600

    Added for loop to organize list

--- main.js
+++ main.js
@@ -1,18 +1,46 @@
 // Nickname Generator by Parivesh
 
-// Get variables from HTML
-let nickBtn = document.getElementById("randNickBtn");
-let allNickBtn = document.getElementById("allNickBtn");
-let firstName = document.getElementById("firstN");
-let lastName = document.getElementById("lastN");
-
 // Global variables and Arrays
-let nicknames = ['Crusher','the Scientist','Twinkle-toes','the Coder','the Jester', 'the Sloth','Quick-Silver']
-
-// Add Event Listener
-nickBtn.addEventListener("click", addNick);
-
-// Process
-function addNick(){
-    document.getElementById("output").innerHTML = firstName + nicknames + lastName;
-}
+let nicknames = [
+    "Crusher",
+    "the Scientist",
+    "Twinkle-toes",
+    "the Coder",
+    "the Jester",
+    "the Sloth",
+    "Quick-Silver"
+  ];
+  
+  // Add Event Listener
+  document.getElementById("randNickBtn").addEventListener("click", addNick);
+  
+  // Process & Output for Random Nicknames
+function addNick() {
+    let firstName = document.getElementById("firstN").value;
+    let lastName = document.getElementById("lastN").value;
+    document.getElementById("output").innerHTML = `${firstName} “${
+      nicknames[randInt(0, nicknames.length)]
+    }” ${lastName}`;
+  }
+  
+// Add Random Integer Generator
+function randInt(low, high) {
+    return Math.floor(Math.random() * (high - low) + low);
+  }
+  
+  // Process & Output for list of all Nicknames
+  document.getElementById("allNickBtn").addEventListener("click", nickList);
+  
+function nickList() {
+    let firstName = document.getElementById("firstN").value;
+    let lastName = document.getElementById("lastN").value;
+  
+    document.getElementById("output").innerHTML = "";
+  
+    for (let i = 0; i < nicknames.length; i++) {
+      document.getElementById(
+        "output"
+      ).innerHTML += `<p> ${firstName} “${nicknames[i]}” ${lastName} </p>`;
+    }
+  }
+  
