381301e77ba105023ab567dd30572da4bb78e82c
Parivesh2005 <108748552+Parivesh2005@users.noreply.github.com>
2022-07-14 12:02:53 -0600

    Organized

--- main.js
+++ main.js
@@ -1,26 +1,16 @@
 // Nickname Generator by Parivesh
 
 // Global variables and Arrays
-let nicknames = [
-    "Crusher",
-    "the Scientist",
-    "Twinkle-toes",
-    "the Coder",
-    "the Jester",
-    "the Sloth",
-    "Quick-Silver"
-  ];
+let nicknames = ["Crusher","the Scientist","Twinkle-toes","the Coder","the Jester","the Sloth","Quick-Silver"];
   
-  // Add Event Listener
+// Add Event Listener
   document.getElementById("randNickBtn").addEventListener("click", addNick);
   
-  // Process & Output for Random Nicknames
+// Process & Output for Random Nicknames
 function addNick() {
     let firstName = document.getElementById("firstN").value;
     let lastName = document.getElementById("lastN").value;
-    document.getElementById("output").innerHTML = `${firstName} “${
-      nicknames[randInt(0, nicknames.length)]
-    }” ${lastName}`;
+    document.getElementById("output").innerHTML = `${firstName} “${nicknames[randInt(0, nicknames.length)]}” ${lastName}`;
   }
   
 // Add Random Integer Generator
@@ -28,19 +18,14 @@ function randInt(low, high) {
     return Math.floor(Math.random() * (high - low) + low);
   }
   
-  // Process & Output for list of all Nicknames
+// Process & Output for list of all Nicknames
   document.getElementById("allNickBtn").addEventListener("click", nickList);
   
 function nickList() {
-    let firstName = document.getElementById("firstN").value;
-    let lastName = document.getElementById("lastN").value;
-  
     document.getElementById("output").innerHTML = "";
   
     for (let i = 0; i < nicknames.length; i++) {
-      document.getElementById(
-        "output"
-      ).innerHTML += `<p> ${firstName} “${nicknames[i]}” ${lastName} </p>`;
+      document.getElementById("output").innerHTML += `<p> “${nicknames[i]}”</p>`;
     }
   }
   
