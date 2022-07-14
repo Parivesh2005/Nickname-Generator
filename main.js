d6c8da79bdaf10c6093de778cfcd283a82c6bfe6
Parivesh2005 <108748552+Parivesh2005@users.noreply.github.com>
2022-07-14 09:43:46 -0600

    Updated Js

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 // Nickname Generator by Parivesh
 
 // Get variables from HTML
-let outputSpan = document.getElementById("output")
-let 
+let nickBtn = document.getElementById("randNickBtn");
+let allNickBtn = document.getElementById("allNickBtn");
+let firstName = document.getElementById("firstN");
+let lastName = document.getElementById("lastN");
+
+// Global variables and Arrays
+let nicknames = ['Crusher','the Scientist','Twinkle-toes','the Coder','the Jester', 'the Sloth','Quick-Silver']
+
+// Add Event Listener
+nickBtn.addEventListener("click", addNick);
+
+// Process
+function addNick(){
+    document.getElementById("output").innerHTML = firstName + nicknames + lastName;
+}
